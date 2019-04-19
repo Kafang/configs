d105bbe2f78049c2de273a0e6cd3d6d696232c1f
kai <wegwerfadresse2720@protonmail.com>
2019-04-19 05:34:48 +0200

    config changed

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #My script for configuring my desktop and workflow
-
+#Dont blame me for calling this configure.sh it does not configure the project...
 
 
 cp config ~/.config/i3/config
