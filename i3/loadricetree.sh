6e19c1e23ea5c4d668b882ceae656757d5ecdf4e
kai <wegwerfadresse2720@protonmail.com>
2020-04-26 14:46:30 +0200

    startup with terminal combo

--- i3/loadricetree.sh
+++ i3/loadricetree.sh
@@ -7,5 +7,4 @@ i3-msg 'workspace "1: Home" ; append_layout ~/.config/i3/workspace1.i3'
 (urxvt -blr 10x3 -tr -sh 35 +sb &)
 (urxvt -blr 10x3 -tr -sh 35 +sb &)
 
-i3-msg 'workspace "2: Browser" ; append_layout ~/.config/i3/workspace2.i3; exec firefox '
 
